e72087c728b90c7ea5ecc41a8eac05a6b9b26a00
tomas-barros1 <tomasabbarros3@gmail.com>
2024-05-11 19:24:35 -0300

    Desafio concluído

---
+++ Estacionamento.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    public class Estacionamento
+{
+    private decimal precoInicial { get; set; }
+    private decimal precoPorHora { get; set; }
+    private List<string> veiculos = new List<string>();
+
+    public Estacionamento(decimal precoInicial, decimal precoPorHora)
+    {
+        this.precoInicial = precoInicial;
+        this.precoPorHora = precoPorHora;
+    }
+
+    public void AdicionarVeiculo(string placa)
+    {
+        veiculos.Add(placa);
+    }
+
+    public void RemoverVeiculo(string placa)
+    {
+        if (veiculos != null)
+        {
+            foreach (var veiculo in veiculos)
+            {
+                if (veiculo == placa)
+                {
+                    Console.WriteLine("Qual foi a quantidade horas que você ficou no estacionamento? ");
+                    decimal horas = decimal.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Valor para pagar: " + (precoPorHora * horas) + precoInicial);
+                } else
+                {
+                    Console.WriteLine("Veículo não encontrado!");
+                }
+            }
+        } else
+        {
+            Console.WriteLine("Nenhum veículo estacionado");
+        }
+
+    }
+
+    public void ListarVeiculos()
+    {
+        if (veiculos.Count == 0)
+        {
+            Console.WriteLine("Nenhum veículo no estacionamento!");
+        }
+        else
+        {
+            foreach (var veiculo in veiculos)
+            {
+                Console.WriteLine(veiculo);
+            }
+        }
+    }
+
+
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿Estacionamento estacionamento = new Estacionamento(10, 2);
+
+void Menu()
+{
+    int opcao;
+    do
+    {
+        Console.WriteLine("1- Cadastrar veículo\n2- Remover veículo\n3- Listar veículos\n4- Encerrar");
+        opcao = int.Parse(Console.ReadLine());
+
+        switch (opcao)
+        {
+            case 1:
+                Console.WriteLine("Digite a placa do veículo: ");
+                string placa = Console.ReadLine();
+                estacionamento.AdicionarVeiculo(placa);
+                break;
+            case 2:
+                Console.WriteLine("Digite a placa do veículo: ");
+                string placaRemover = Console.ReadLine();
+                estacionamento.RemoverVeiculo(placaRemover);
+                break;
+            case 3:
+                Console.WriteLine("Lista de carros: ");
+                estacionamento.ListarVeiculos();
+                break;
+            default:
+                Console.WriteLine("Opção inválida!");
+                break;
+        }
+    } while (opcao != 4);
+}
+
+Menu();
